57c91409398ed70736006ed4a8d75a1417114a6f
Kevin Lessard <kevinlessard74@gmail.com>
2023-09-23 12:14:25 -0400

    fixed clipboard issues

--- .config/nvim/lua/hazot/set.lua
+++ .config/nvim/lua/hazot/set.lua
@@ -10,6 +10,8 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
+vim.opt.clipboard = 'unnamedplus'
+
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
