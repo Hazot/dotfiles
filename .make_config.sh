3462a016b317329608e1b2b494822e198a66e252
Kevin Lessard <kevinlessard74@gmail.com>
2023-09-24 12:54:50 -0400

    Delete .make_config.sh

--- .make_config.sh
+++
@@ -1,8 +0,0 @@
-for i in $(seq 1 9)
-do
-    echo "['<Super>${i}']";
-    gsettings set org.gnome.shell.keybindings switch-to-application-${i} "['<Super>${i}']";
-    gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-${i} "['<Control><Super>${i}']";
-done
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-${0} "['<Control><Super>${0}']";
-echo "Done"
