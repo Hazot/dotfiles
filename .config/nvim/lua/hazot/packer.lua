d153db0e4204e3bdd8baabbe7a8a451dc6dfc462
Kevin Lessard <kevinlessard74@gmail.com>
2023-10-05 22:59:53 -0400

    added gitgutter

--- .config/nvim/lua/hazot/packer.lua
+++ .config/nvim/lua/hazot/packer.lua
@@ -51,4 +51,14 @@ return require('packer').startup(function(use)
             'nvim-tree/nvim-web-devicons', -- optional
         },
     }
+    use({
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+    })
+    use('airblade/vim-gitgutter')
 end)
