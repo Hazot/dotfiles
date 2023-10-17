3e2bbb88342e3578bb376c34cf81de1ca89a7bb2
Kevin Lessard <kevinlessard74@gmail.com>
2023-10-16 20:24:15 -0400

    added luasnip with no snip

--- .config/nvim/lua/hazot/packer.lua
+++ .config/nvim/lua/hazot/packer.lua
@@ -61,4 +61,11 @@ return require('packer').startup(function(use)
     end
     })
     use('airblade/vim-gitgutter')
+    use({
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!:).
+        run = "make install_jsregexp"
+    })
 end)
