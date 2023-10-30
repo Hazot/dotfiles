79fbfcde727b2985ba7d268c290735fbee637177
Kevin Lessard <kevinlessard74@gmail.com>
2023-10-30 15:29:49 -0400

    upd more paresseux

--- .config/aliases.sh
+++ .config/aliases.sh
@@ -8,6 +8,10 @@ alias python='python3'
 alias vi='nvim'
 alias fd='fd --no-ignore'
 
+# Custom update function call
+alias upd='updapack'
+alias upd='upda'
+
 # Alias for listing directories 
 alias ls='ls --color=auto'
 alias ll="ls -lAh --group-directories-first --color=auto"
