f6287c84a3956908aa584837441dda7af73c16bd
Kevin Lessard <kevinlessard74@gmail.com>
2023-11-16 17:04:59 -0500

    remap + nvimtree

--- .config/nvim/lua/hazot/remap.lua
+++ .config/nvim/lua/hazot/remap.lua
@@ -37,3 +37,6 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 -- Have j and k working with word wrap
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr=true, silent=true} )
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr=true, silent=true} )
+
+-- Nvim Tree
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
