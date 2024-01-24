64193e6680c311cefb28533c73d795b1ffccc777
Kevin Lessard <kevinlessard74@gmail.com>
2024-01-24 14:02:10 -0500

    nvim update

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -2,13 +2,13 @@ local lspconfig = require('lspconfig')
 local lsp_zero = require('lsp-zero')
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 cmp.setup({
     sources = {
-        {name = 'path'},
-        {name = 'nvim_lsp'},
-        {name = 'nvim_lua'},
+        { name = 'path' },
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lua' },
     },
 
     -- Make the first item in completion menu always be selected.
@@ -27,14 +27,14 @@ cmp.setup({
 })
 
 lsp_zero.set_preferences({
-	sign_icons = { }
+    sign_icons = {}
 })
 
 lsp_zero.on_attach(function(client, bufnr)
     -- see :help lsp-zero-keybindings
     -- to learn the available actions
-    lsp_zero.default_keymaps({buffer = bufnr})
-    local opts = {buffer = bufnr, remap = false}
+    lsp_zero.default_keymaps({ buffer = bufnr })
+    local opts = { buffer = bufnr, remap = false }
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
@@ -49,23 +49,23 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {
-      "pyright",
-      "lua_ls",
-      "rust_analyzer",
-      "clangd"
-  },
-  handlers = {
-    lsp_zero.default_setup,
-    lua_ls = function ()
-        local lua_opts = lsp_zero.nvim_lua_ls()
-        lspconfig.lua_ls.setup(lua_opts)
-    end,
-  }
+    ensure_installed = {
+        "pyright",
+        "lua_ls",
+        "rust_analyzer",
+        "clangd"
+    },
+    handlers = {
+        lsp_zero.default_setup,
+        lua_ls = function()
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            lspconfig.lua_ls.setup(lua_opts)
+        end,
+    }
 })
 
-lspconfig.clangd.setup{}
-lspconfig.pyright.setup{}
+lspconfig.clangd.setup {}
+lspconfig.pyright.setup {}
 lspconfig.rust_analyzer.setup({})
 
 lsp_zero.setup()
--- .config/nvim/lua/hazot/remap.lua
+++ .config/nvim/lua/hazot/remap.lua
@@ -8,8 +8,8 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- Does not move mouse when appending lines with J
 vim.keymap.set("n", "J", "mzJ`z")
 
-vim.keymap.set("n", "<C-d>","<C-d>zz")
-vim.keymap.set("n", "<C-u>","<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
@@ -18,7 +18,7 @@ vim.keymap.set("x", "<leader>p", "\"_dP")
 
 
 -- Yank multiple lines to clipboard easily
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 -- Only difference between escape and ctrl+c
@@ -35,16 +35,16 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Have j and k working with word wrap
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr=true, silent=true} )
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr=true, silent=true} )
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Can use tab for block indentation (not sure why it overrides <C-I>)
 local opts = { noremap = true, silent = true }
 --[[ vim.keymap.set("n",    "<Tab>",         ">>",  opts)
 vim.keymap.set("n",    "<S-Tab>",       "<<",  opts) ]]
-vim.keymap.set("v",    "<Tab>",         ">gv", opts)
-vim.keymap.set("v",    "<S-Tab>",       "<gv", opts)
+vim.keymap.set("v", "<Tab>", ">gv", opts)
+vim.keymap.set("v", "<S-Tab>", "<gv", opts)
 
--- Format on key press (one from 
+-- Format on key press (one from
 vim.keymap.set("n", "<leader>wf", vim.lsp.buf.format, { remap = false })
 vim.keymap.set("n", "<C-I>", vim.lsp.buf.format, { remap = false })
